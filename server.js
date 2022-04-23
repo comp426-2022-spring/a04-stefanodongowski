123be9116565e376563f629019bce54c81c2abbf
Stefano Dongowski <stefano@unc.edu>
2022-04-23 13:05:38 -0400

    fixed --log=false

--- server.js
+++ server.js
@@ -42,32 +42,6 @@ const server = app.listen(port, () => {
     console.log(`App is running on port ${port}`)
 })
 
-app.use( (req, res, next) => {
-    let logdata = {
-        remoteaddr: req.ip,
-        remoteuser: req.user,
-        time: Date.now(),
-        method: req.method,
-        url: req.url,
-        protocol: req.protocol,
-        httpversion: req.httpVersion,
-        status: res.statusCode,
-        referer: req.headers['referer'],
-        useragent: req.headers['user-agent']
-    }
-
-    const stmt = logdb.prepare(`
-        INSERT INTO accesslog (remoteaddr, remoteuser, time, method,
-            url, protocol, httpversion, status, referer, useragent)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-    `)
-
-    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time,
-        logdata.method, logdata.url, logdata.protocol, logdata.httpversion,
-        logdata.status, logdata.referer, logdata.useragent)
-    next()
-})
-
 if (debug) {
     app.get('/app/log/access/', (res, req, next) => {
         const stmt = logdb.prepare('SELECT * FROM accesslog').all()
@@ -80,7 +54,32 @@ if (debug) {
     })
 }
 
-if (log) {
+if (log !== false) {
+    app.use( (req, res, next) => {
+        let logdata = {
+            remoteaddr: req.ip,
+            remoteuser: req.user,
+            time: Date.now(),
+            method: req.method,
+            url: req.url,
+            protocol: req.protocol,
+            httpversion: req.httpVersion,
+            status: res.statusCode,
+            referer: req.headers['referer'],
+            useragent: req.headers['user-agent']
+        }
+    
+        const stmt = logdb.prepare(`
+            INSERT INTO accesslog (remoteaddr, remoteuser, time, method,
+                url, protocol, httpversion, status, referer, useragent)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        `)
+    
+        const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time,
+            logdata.method, logdata.url, logdata.protocol, logdata.httpversion,
+            logdata.status, logdata.referer, logdata.useragent)
+        next()
+    })
     const accesslog = fs.createWriteStream('./access.log', { flags: 'a' })
     // Set up the access logging middleware
     app.use(morgan('combined', { stream: accesslog }))
